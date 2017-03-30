dbf2195ec259cc7707de9ee59fe04756546e7c96
Nikita Koshelev <nikita.koshelev@gmail.com>
2017-03-30 22:27:40 +0300

    Refactor some functions

--- calculation.py
+++ calculation.py
@@ -2,15 +2,18 @@
 from __future__ import unicode_literals, division
 
 from datetime import timedelta
-from math import sqrt, sin, cos, pi, atan2
+from math import sqrt, sin, cos, atan2, degrees, radians
 
 import ephem
 import numpy as np
+from collections import namedtuple
 from haversine import haversine
 from sgp4.ext import jday as days
 
-
 from tle import TLE
+
+ECEF = namedtuple('ECEF', ('x', 'y', 'z'))
+LLA = namedtuple('LLA', ('lat', 'lon', 'alt'))
 
 _tle = TLE()
 
@@ -22,8 +25,7 @@ def sun_angle(dot, t):
     obs.date = t
     sun = ephem.Sun(obs)
     sun.compute(obs)
-    sun_angle = float(sun.alt) * 57.2957795  # Convert Radians to degrees
-    return sun_angle
+    return degrees(sun.alt)
 
 
 def normalize(v):
@@ -31,6 +33,45 @@ def normalize(v):
     if norm == 0:
         return v
     return v / norm
+
+
+def ecef2lla(x, y, z):
+    # WGS84 ellipsoid constants:
+    a = 6378137
+    e = 8.1819190842622e-2
+
+    # calculations:
+    b = sqrt(a ** 2 * (1 - e ** 2))
+    ep = sqrt((a ** 2 - b ** 2) / b ** 2)
+    p = sqrt(x ** 2 + y ** 2)
+    th = atan2(a * z, b * p)
+    lon = atan2(y, x)
+    lat = atan2((z + ep ** 2 * b * sin(th) ** 3), (p - e ** 2 * a * cos(th) ** 3))
+    N = a / sqrt(1 - e ** 2 * sin(lat) ** 2)
+    alt = p / cos(lat) - N
+    return LLA(degrees(lat), degrees(lon), alt)
+
+
+def lla2ecef(lat, lon, alt, rad=False):
+    """
+    """
+    if not rad:
+        lat, lon = radians(lat), radians(lon)
+
+    # WGS84 ellipsoid constants:
+    a = 6378137
+    e = 8.1819190842622e-2
+
+    # intermediate calculation
+    # (prime vertical radius of curvature)
+    N = a / sqrt(1 - e ** 2 * sin(lat) ** 2)
+
+    # results:
+    x = (N + alt) * cos(lat) * cos(lon)
+    y = (N + alt) * cos(lat) * sin(lon)
+    z = ((1 - e ** 2) * N + alt) * sin(lat)
+
+    return ECEF(x, y, z)
 
 
 # Задает и поворачивает единичный вектор
@@ -51,9 +92,9 @@ def origin_direction(position, velocity, angle_x, angle_y, angle_z, GMST_rotatio
                               [Xiss[0, 1], Yiss[0, 1], Ziss[0, 1]],
                               [Xiss[0, 2], Yiss[0, 2], Ziss[0, 2]]])
 
-    angle_x = angle_x * pi / 180.0  # rotation around X axis
-    angle_y = angle_y * pi / 180.0  # rotation around Y axis
-    angle_z = angle_z * pi / 180.0  # rotation around Z axis
+    angle_x = radians(angle_x)
+    angle_y = radians(angle_y)
+    angle_z = radians(angle_z)
     ca = cos(angle_x)
     sa = sin(angle_x)
     cb = cos(angle_y)
@@ -74,10 +115,7 @@ def origin_direction(position, velocity, angle_x, angle_y, angle_z, GMST_rotatio
     Mz = np.matrix([[cc, -sc, 0],
                     [+sc, cc, 0],
                     [0, 0, 1]])
-    iss_orientation_matrix = np.dot(Mx, My)
-    iss_orientation_matrix = np.dot(iss_orientation_matrix, My)
-    iss_orientation_matrix = np.dot(iss_orientation_matrix, Mz)
-    iss_orientation_matrix = np.dot(iss_orientation_matrix, additional_rotation_matrix)
+    iss_orientation_matrix = np.dot(np.dot(np.dot(np.dot(Mx, My), My), Mz), additional_rotation_matrix)
     # Перейдем в iss
     direction = np.dot(direction, shift_matrix)
     # Повернем вектор направления
@@ -90,9 +128,10 @@ def origin_direction(position, velocity, angle_x, angle_y, angle_z, GMST_rotatio
 def calc_intersection(x, y, z, u, v, w):
     # u,v,w - это координаты ecef
     # define WGS-84
-    a = 6378137.0 / 1000.0  # semi-major axis of the WGS-84 ellipsoid in meters
+
+    a = 6378137.0  # semi-major axis of the WGS-84 ellipsoid in meters
     b = a  # semi-minor axis in meters
-    c = 6356752.3142 / 1000.0
+    c = 6356752.3142
     sq_eq_a = u ** 2 * b ** 2 * c ** 2 + v ** 2 * a ** 2 * c ** 2 + w ** 2 * a ** 2 * c ** 2
     sq_eq_b = 2 * (x * u * b ** 2 * a ** 2 + y * v * a ** 2 * c ** 2 + z * w * a ** 2 * b ** 2)
     sq_eq_c = (x ** 2 * b ** 2 * c ** 2
@@ -116,14 +155,6 @@ def calc_intersection(x, y, z, u, v, w):
     return hit
 
 
-# Считает JD
-def days(year, month, day, hours, minutes, seconds):
-    dwhole = 367 * year - 7 * (year + (month + 9) // 12) // 4 + 275 * month // 9 + day - 730531.5
-    dfrac = (hours + minutes / 60 + seconds / 3600) / 24
-    d = dwhole + dfrac
-    return d
-
-
 # Считает Гринвичское среднее звездное время, то есть угол поворота вращающейся СК относительно инерциальной
 def GMST(year, month, day, hours, minutes, seconds):
     d = days(year, month, day, hours, minutes, seconds)
@@ -131,7 +162,7 @@ def GMST(year, month, day, hours, minutes, seconds):
     GMST = GMST - 360 * int(GMST / 360)
     if GMST < 0:
         GMST = 360.0 + GMST
-    GMST = GMST * pi / 180
+    GMST = radians(GMST)
     cz = cos(GMST)
     sz = sin(GMST)
     # Поворот относительно оси z - из центра Земли в сев. полюс
@@ -141,42 +172,6 @@ def GMST(year, month, day, hours, minutes, seconds):
         [0, 0, 1]
     ])
     return GMST, Rz
-
-
-def convert_ll_to_xyz(geo_coords):
-    # see http://www.mathworks.de/help/toolbox/aeroblks/llatoecefposition.html
-    (lat, lon) = geo_coords
-    alt = 0
-    rad = np.float64(6378137.0)  # Radius of the Earth (in meters)
-    f = np.float64(1.0 / 298.257223563)  # Flattening factor WGS84 Model
-    cosLat = np.cos(lat)
-    sinLat = np.sin(lat)
-    FF = (1.0 - f) ** 2
-    C = 1 / np.sqrt(cosLat ** 2 + FF * sinLat ** 2)
-    S = C * FF
-    x = (rad * C + alt) * cosLat * np.cos(lon)
-    y = (rad * C + alt) * cosLat * np.sin(lon)
-    z = (rad * S + alt) * sinLat
-    return x, y, z
-
-
-def convert_xyz_ll(coords):
-    # WGS-95 ellipsoid constants
-    (x, y, z) = coords
-    a = 6378137.0 / 1000.0
-    e = 8.1819190842622e-2
-    e = 0.0818191909289062
-
-    # calculations
-    b = sqrt(a ** 2 * (1 - e ** 2))
-    ep = sqrt((a ** 2 - b ** 2) / b ** 2)
-    p = sqrt(x ** 2 + y ** 2)
-    th = atan2(a * z, b * p)
-    lon = atan2(y, x)
-    lat = atan2((z + ep ** 2 * b * sin(th) ** 3), (p - e ** 2 * a * cos(th) ** 3))
-    N = a / sqrt(1 - e ** 2 * sin(lat) ** 2)
-    alt = p / cos(lat) - N
-    return lat * 180.0 / pi, lon * 180.0 / pi
 
 
 def distance(dot_1, dot_2):
@@ -216,10 +211,13 @@ def geolocate(position, velocity, alfa, beta, gamma, t):
     origin, direction = origin_direction(position, velocity, alfa, beta, gamma, GMST_rotation_matrix, 0)
     hit_1 = calc_intersection(origin[0, 0], origin[0, 1], origin[0, 2],
                               direction[0, 0], direction[0, 1], direction[0, 2])
-    return convert_xyz_ll(hit_1)
+    return ecef2lla(*hit_1) if hit_1 else hit_1
 
 
 def hit_the_earth(satellite, t):
     position, velocity = satellite.propagate(t.year, t.month, t.day, t.hour, t.minute, t.second)
     return geolocate(position, velocity, 0, 0, 0, t)
 
+
+if __name__ == '__main__':
+    print(ecef2lla(*lla2ecef(45, -200, 1000)))
